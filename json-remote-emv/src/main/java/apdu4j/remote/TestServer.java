e3f51bf00f88f26c337fa71df2259907f3040387
Martin Paljak <martin@martinpaljak.net>
2019-05-08 11:42:16 +0300

    Update metacard parent and fix javadoc mismatches

--- json-remote-emv/src/main/java/apdu4j/remote/TestServer.java
+++ json-remote-emv/src/main/java/apdu4j/remote/TestServer.java
@@ -21,6 +21,12 @@
  */
 package apdu4j.remote;
 
+import apdu4j.HexUtils;
+import apdu4j.remote.RemoteTerminal.Button;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.smartcardio.*;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -29,80 +35,65 @@ import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import javax.smartcardio.CardChannel;
-import javax.smartcardio.CardException;
-import javax.smartcardio.CardTerminal;
-import javax.smartcardio.CommandAPDU;
-import javax.smartcardio.ResponseAPDU;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import apdu4j.HexUtils;
-import apdu4j.remote.RemoteTerminal.Button;
-import apdu4j.remote.RemoteTerminal.UserCancelExcption;
-
 // Sample test server for remote terminal.
 public class TestServer extends RemoteTerminalThread {
-	private static Logger logger = LoggerFactory.getLogger(TestServer.class);
+    private static Logger logger = LoggerFactory.getLogger(TestServer.class);
 
-	// Start a socket test server.
-	static void start(ServerSocket socket) {
-		ExecutorService executor = Executors.newWorkStealingPool();
-		while(true) {
-			try {
-				Socket s = socket.accept();
-				SocketTransport transport = new SocketTransport(s);
-				TestServer client = new TestServer();
-				client.setTerminal(new RemoteTerminal(transport));
-				executor.execute(client);
-			} catch (IOException e) {
-				logger.trace("Could not accept client", e);
-			}
-		}
-	}
+    // Start a socket test server.
+    static void start(ServerSocket socket) {
+        ExecutorService executor = Executors.newWorkStealingPool();
+        while (true) {
+            try {
+                Socket s = socket.accept();
+                SocketTransport transport = new SocketTransport(s);
+                TestServer client = new TestServer();
+                client.setTerminal(new RemoteTerminal(transport));
+                executor.execute(client);
+            } catch (IOException e) {
+                logger.trace("Could not accept client", e);
+            }
+        }
+    }
 
-	private void setTerminal(RemoteTerminal t) {
-		terminal = t;
-	}
+    private void setTerminal(RemoteTerminal t) {
+        terminal = t;
+    }
 
-	// Run a sample session for a client.
-	@Override
-	public void run() {
-		logger.info("Started session");
-		try {
-			terminal.start();
-			terminal.statusMessage("Welcome!");
-			String name = terminal.input("Please enter your name");
-			terminal.statusMessage("Hello, " + name);
-			Map<Integer, String> chs = new HashMap<>();
-			chs.put(1, "Apple");
-			chs.put(2, "Orange");
-			chs.put(3, "Banana");
-			chs.put(4, "Hammer");
-			int ch = terminal.select("What do you like?", chs);
-			terminal.statusMessage("So you like " + ch);
+    // Run a sample session for a client.
+    @Override
+    public void run() {
+        logger.info("Started session");
+        try {
+            terminal.start();
+            terminal.statusMessage("Welcome!");
+            String name = terminal.input("Please enter your name");
+            terminal.statusMessage("Hello, " + name);
+            Map<Integer, String> chs = new HashMap<>();
+            chs.put(1, "Apple");
+            chs.put(2, "Orange");
+            chs.put(3, "Banana");
+            chs.put(4, "Hammer");
+            int ch = terminal.select("What do you like?", chs);
+            terminal.statusMessage("So you like " + ch);
 
-			try {
+            try {
 
-				CardTerminal ct = terminal.getCardTerminal();
-				CardChannel c = ct.connect("*").getBasicChannel();
+                CardTerminal ct = terminal.getCardTerminal();
+                CardChannel c = ct.connect("*").getBasicChannel();
 
-				if (terminal.dialog("Shall we try to select MF in " + ct.getName()).equals(Button.GREEN)) {
-					ResponseAPDU r = c.transmit(new CommandAPDU(0x00, 0xA4, 0x04, 0x00));
-					terminal.statusMessage("Card returned: " + HexUtils.bin2hex(r.getBytes()));
-				}
+                if (terminal.dialog("Shall we try to select MF in " + ct.getName()).equals(Button.GREEN)) {
+                    ResponseAPDU r = c.transmit(new CommandAPDU(0x00, 0xA4, 0x04, 0x00));
+                    terminal.statusMessage("Card returned: " + HexUtils.bin2hex(r.getBytes()));
+                }
 
-				terminal.input("Please enter your name");
-			} catch (CardException e) {
-				terminal.statusMessage("Failed: " + e.getMessage());
-			} finally {
-				terminal.stop("Bye!");
-			}
-		} catch (IOException e) {
-			logger.error("Communication error", e);
-		} catch (UserCancelExcption e) {
-			logger.warn("User cancelled the operation with red button");
-		}
-	}
+                terminal.input("Please enter your name");
+            } catch (CardException e) {
+                terminal.statusMessage("Failed: " + e.getMessage());
+            } finally {
+                terminal.stop("Bye!");
+            }
+        } catch (IOException e) {
+            logger.error("Communication error", e);
+        }
+    }
 }
