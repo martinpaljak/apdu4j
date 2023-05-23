1e6df39594bf9a56a25a0748259e60f10c9caba2
Martin Paljak <martin@martinpaljak.net>
2023-05-23 10:11:34 +0300

    Please compiler. The whole approach is garbage, so to be removed.

--- core/src/main/java/apdu4j/core/SmartCardAppListener.java
+++ core/src/main/java/apdu4j/core/SmartCardAppListener.java
@@ -60,6 +60,8 @@ public interface SmartCardAppListener extends SmartCardApp {
 
     // Data objects
     class AppParameters extends HashMap<String, String> {
+        public AppParameters() {
+        }
         private static final long serialVersionUID = 5410274086433485297L;
         // Set to "true" to support multiple sessions in one app
         public static final String MULTISESSION_BOOLEAN = "multisession";
@@ -70,6 +72,8 @@ public interface SmartCardAppListener extends SmartCardApp {
     }
 
     class CardData extends HashMap<String, Object> {
+        public CardData() {
+        }
         private static final long serialVersionUID = 2127675255938833899L;
         public static final String PROTOCOL_STRING = "protocol";
         public static final String ATR_BYTES = "atr";
